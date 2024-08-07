b49a4e03f44807ac7c9df8c7bf6aac87a8cb834a
Guisposito <guicarlosjoinhas@gmail.com>
2024-08-07 11:04:55 -0300

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import {Header , Footer} from "../components";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Guilherme Sposito",
+  description: "Meu Portifolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${inter.className} bg-slate-950 text-white`}>
+        <Header />
+        <Footer />
+          {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <div className="container mx-auto px-4">
+                <div className="main-menu">
+                    <div className="logo">
+                        
+                    </div>
+                    <div className="menu-desktop">
+                        <ul>
+                            <li>Projetos</li>
+                            <li>Experiências</li>
+                            <li>Contato</li>
+                        </ul>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Header
